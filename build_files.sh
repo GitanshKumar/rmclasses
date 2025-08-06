86724b0e2cac9ab78037e9f397c405be6b1e0370
GitanshKumar <gitanshkumar5@gmail.com>
2025-08-06 18:48:58 +0530

    Add vercel setup - 2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
-pip install -r requirements.txt
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic --noinput
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
 echo "BUILD END"
