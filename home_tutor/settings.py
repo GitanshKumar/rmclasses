2ae549a654d5d485e8e0dcd0b0c9789594de681d
GitanshKumar <gitanshkumar5@gmail.com>
2025-12-30 12:48:05 +0530

    deployment settings update

--- home_tutor/settings.py
+++ home_tutor/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") != "False"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", 'rmclasses.com']
 
 CSRF_TRUSTED_ORIGINS = ["http://*", "https://*"]
 
